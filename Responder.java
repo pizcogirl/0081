3fc2157219f06034713c72792855d015e54ceb27
Julia Zuara <julia_any@hotmail.com>
2015-02-23 10:47:54 +0100

    Cambiada la clase responder, añadidos atributos y modificado el constructor

--- Responder.java
+++ Responder.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +8,25 @@
  */
 public class Responder
 {
+    // Random para seleccionar respuestas
+    private Random rnd;
+    // ArrayList de respuestas para elegir
+    private ArrayList<String> respuestas;
+    
     /**
-     * Construct a Responder - nothing to do
+     * Construct a Responder
      */
     public Responder()
     {
+        // Inicializamos los atributos
+        rnd = new Random(System.currentTimeMillis());
+        // Creamos cinco frases para el arrayList
+        respuestas = new ArrayList<String>();
+        respuestas.add("I cannot reproduce that error");
+        respuestas.add("You need to contact you OS support");
+        respuestas.add("Can you repeat it, please?");
+        respuestas.add("Have you tryed rebooting?");
+        respuestas.add("Its not an error, its a feature");
     }
 
     /**
